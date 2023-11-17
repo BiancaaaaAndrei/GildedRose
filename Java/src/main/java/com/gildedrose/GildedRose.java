ec47cb81922907ac2d832d2d9d87b7a17d0fbca3
Bianca Andrei <BAndrei@spartaglobal.com>
2023-11-17 11:45:02 +0000

    Code restructured

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,55 @@
 package com.gildedrose;
-
-class GildedRose {
+import java.util.Arrays;
+public class GildedRose {
     Item[] items;
-
     public GildedRose(Item[] items) {
         this.items = items;
     }
-
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        Arrays.stream(items).forEach(this::updateItem);
+    }
+    private void updateItem(Item item) {
+        if (isNormalItem(item)) updateNormalItem(item);
+        else if (isAgedBrie(item)) updateAgedBrie(item);
+        else if (isBackstagePass(item)) updateBackstagePass(item);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+        if (!isSulfuras(item)) decrementSellIn(item);
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+        if (item.sellIn < 0) handleExpiredItem(item);
+    }
+    private boolean isNormalItem(Item item) {
+        return !isAgedBrie(item) && !isBackstagePass(item) && !isSulfuras(item);
+    }
+    private boolean isAgedBrie(Item item) {
+        return "Aged Brie".equals(item.name);
+    }
+    private boolean isBackstagePass(Item item) {
+        return "Backstage passes to a TAFKAL80ETC concert".equals(item.name);
+    }
+    private boolean isSulfuras(Item item) {
+        return "Sulfuras, Hand of Ragnaros".equals(item.name);
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
+    private void updateNormalItem(Item item) {
+        if (item.quality > 0 && !isSulfuras(item))
+            item.quality--;
+
+    }
+    private void updateAgedBrie(Item item) {
+        if (item.quality < 50) item.quality++;
+    }
+
+    private void updateBackstagePass(Item item){
+        if (item.quality < 50) item.quality++;
+        if (item.sellIn < 11 && item.quality < 50) item.quality++;
+        if (item.sellIn < 6 && item.quality < 50) item.quality++;
+    }
+    private void decrementSellIn(Item item) {
+        item.sellIn--;}
+    private void handleExpiredItem(Item item) {
+        if (isNormalItem(item) && item.quality > 0 && !isSulfuras(item)) item.quality--;
+        else if (isBackstagePass(item)) item.quality = 0;
+        else if (isAgedBrie(item) && item.quality < 50) item.quality++;
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,16 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void updateQuality_NormalItem() {
+        Item[] items = new Item[]{new Item("Normal Item", 5, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(9, app.items[0].quality);
     }
 
 }
